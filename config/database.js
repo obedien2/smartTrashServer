799be4baf0052e5edd81fc0093bea081f758d4df
Obedient Ankrah <obedient.ankrah@gmail.com>
2018-06-13 14:26:21 +0000

    Changed database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    //'url': 'mongodb://nanaKwame:king123@ds117178.mlab.com:17178/idoc'
-    'url': 'mongodb://127.0.0.1/smartTrashTest'
+    'url': 'mongodb://binadmin:admin@admin1@ds259410.mlab.com:59410/smart-trash-server'
+   // 'url': 'mongodb://127.0.0.1/smartTrashTest'
     
 }
