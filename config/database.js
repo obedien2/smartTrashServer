a554491bdb7c74848b207d503bc66bfd4dd115de
Obedient Ankrah <obedient.ankrah@gmail.com>
2018-06-14 17:11:16 +0000

    Made some changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'url': 'mongodb://binadmin:admin@admin1@ds259410.mlab.com:59410/smart-trash-server'
+    'url': 'mongodb://nanaKwame:king123@ds259410.mlab.com:59410/smart-trash-server'
    //'url': 'mongodb://127.0.0.1:27017/smartTrashTest'
     
 }
